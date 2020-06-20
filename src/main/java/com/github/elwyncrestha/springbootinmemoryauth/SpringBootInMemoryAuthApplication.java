109b8f46b4b7d7515d588ebdfa98b48a6d76f71a
Elvin Shrestha <elwyncrestha@gmail.com>
2020-06-20 17:15:39 +0545

    Feature: Added swagger UI.

--- src/main/java/com/github/elwyncrestha/springbootinmemoryauth/SpringBootInMemoryAuthApplication.java
+++ src/main/java/com/github/elwyncrestha/springbootinmemoryauth/SpringBootInMemoryAuthApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.github.elwyncrestha.springbootinmemoryauth.api")
 @EntityScan(basePackages = "com.github.elwyncrestha.springbootinmemoryauth.api")
 @ComponentScan(basePackages = "com.github.elwyncrestha")
+@EnableSwagger2
 public class SpringBootInMemoryAuthApplication {
 
 	public static void main(String[] args) {
